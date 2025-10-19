8aad7877f4b3701b1303cf7787d9e50766355a59
pojiang <2114654816@qq.com>
2025-10-19 13:26:58 +0000

    test

--- function.py
+++ function.py
@@ -11,7 +11,7 @@ def calc_mean_std(feat, eps=1e-5):
     feat_mean = feat.view(N, C, -1).mean(dim=2).view(N, C, 1, 1)
     return feat_mean, feat_std
 
-def calc_mean_std1(feat, eps=1e-5):
+def calc_mean_std1(feat, eps=1e-5):   
     # eps is a small value added to the variance to avoid divide-by-zero.
     size = feat.size()
     # assert (len(size) == 4)
